ca0928b2e335c75945c5076a68d374e354f253f9
korey ko <koreykendrick1@gmail.com>
2020-06-19 04:27:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,11 +19,6 @@ class Scraper
  end
 def get_courses
   self.get_page.css(".post")
-  |post|
-    course = Course.new
-    course.title = post.css("h2").text
-    course.schedule = post.css(".date").text
-    course.description = post.css("p").text
   end
 
 
