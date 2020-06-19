bea970936fcc16a0c234afc608155c072453e6cf
korey ko <koreykendrick1@gmail.com>
2020-06-19 04:10:43 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,10 +3,11 @@ require 'open-uri'
 require "pry"
 
 require_relative './course.rb'
-
+=begin
 class get_page
   doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-  
+ 
+=end 
   def print_courses
     self.make_courses
     Course.all.each do |course|
